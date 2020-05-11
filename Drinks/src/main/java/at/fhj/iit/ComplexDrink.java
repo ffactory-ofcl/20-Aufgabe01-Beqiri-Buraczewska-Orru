202517d654170be3124bf65add1b924e28fb1f65
Fisnik B <fisnik.beqiri@edu.fh-joanneum.at>
2020-05-11 10:41:24 +0200

    Updated Complex Drink Class

--- Drinks/src/main/java/at/fhj/iit/ComplexDrink.java
+++ Drinks/src/main/java/at/fhj/iit/ComplexDrink.java
@@ -12,7 +12,6 @@ public class ComplexDrink extends Drink {
      */
     private List<Liquid> liquids = new ArrayList<>();
 
-<<<<<<< HEAD
 /**
 * Creates a ComplexDrink object with given name
 * 
@@ -43,14 +42,6 @@ public double getVolume() {
     double volume=0;
     for(Liquid l:liquids){
         volume+=l.getVolume();
-=======
-    /**
-     * Creates a ComplexDrink object with given name
-     * 
-     * @param name name of drink
-     */
-    public ComplexDrink(String name) {
-        super(name);
     }
 
     /**
@@ -61,10 +52,8 @@ public double getVolume() {
     public ComplexDrink(String name, Liquid liquid) {
         super(name);
         liquids.add(liquid);
->>>>>>> 8bbb2d104af81233741e9f71d73fbec107282e55
     }
 
-<<<<<<< HEAD
 /**
 * Returns alcohol volume percent of all liquids
 *
@@ -105,71 +94,8 @@ if(liquids.size()==10){
     throw new TooManyIngredientsException(liquid);
 }else{
     liquids.add(liquid);}
-=======
-    /**
-     * Returns volume of all liquids
-     *
-     * @return the volume of drink in litre
-     */
-    @Override
-    public double getVolume() {
-        double volume = 0;
-        for (Liquid l : liquids) {
-            volume += l.getVolume();
-        }
-        return volume;
-    }
+}
 
-    /**
-     * Returns alcohol volume percent of all liquids
-     *
-     * @return alcohol volume percent
-     */
-    @Override
-    public double getAlcoholPercent() {
-        double percent = 0;
-        for (Liquid l : liquids) {
-            percent += l.getAlcoholPercent();
-        }
-        return percent;
-    }
-
-    /**
-     * Gives information if drink is alcoholic or not
-     *
-     * @return true when alcoholic liquids are present, otherwise false
-     */
-    @Override
-    public boolean isAlcoholic() {
-        if (getAlcoholPercent() > 0) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Adds a liquid to the drink
-     * 
-     * @param liquid the liquid to add
-     * @throws TooManyIngredientsException exception could be thrown
-     */
-    public void addLiquid(Liquid liquid) throws TooManyIngredientsException {
->>>>>>> 8bbb2d104af81233741e9f71d73fbec107282e55
-
-        if (liquids.size() == 2) {
-            System.out.println("here");
-            throw new TooManyIngredientsException(liquid);
-        } else {
-            liquids.add(liquid);
-        }
-
-<<<<<<< HEAD
-/**
- * Class Exception for the limit of liquids in a drink
- */
-class TooManyIngredientsException extends Exception{
-=======
-    }
 
     /**
      * Class Exception for the limit of liquids in a drink
@@ -179,7 +105,6 @@ class TooManyIngredientsException extends Exception{
         public TooManyIngredientsException(Liquid liquid) {
             super("Drink has too many Ingredients.\nLiquid " + liquid.getName() + " was not added.");
         }
->>>>>>> 8bbb2d104af81233741e9f71d73fbec107282e55
 
     }
 }
