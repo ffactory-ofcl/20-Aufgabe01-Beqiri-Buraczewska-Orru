a7801d6d271be1ac3612acd0afe6ba65a27829f9
Filippo Orrù <filippoorru@hotmail.com>
2020-05-12 15:30:01 +0200

    ComplexDrink: Fix remaining merge errors - Add constructor that takes List of Liquid - Fix typo - Fix formatting - Move Exception out of class

--- Drinks/src/main/java/at/fhj/iit/ComplexDrink.java
+++ Drinks/src/main/java/at/fhj/iit/ComplexDrink.java
@@ -8,103 +8,105 @@ import java.util.List;
  */
 public class ComplexDrink extends Drink {
     /**
-     * class uses up to 10 luqids;
+     * class uses up to 10 liqids;
      */
     private List<Liquid> liquids = new ArrayList<>();
 
-/**
-* Creates a ComplexDrink object with given name
-* 
-* @param name name of drink
-*/
-public ComplexDrink(String name) {
-		super(name);
-}
-
-/**
-* Creates a ComplexDrink object with given name
-* and liquid
-* 
-* @param name name of drink
-*/
-public ComplexDrink(String name,Liquid liquid) {
+    /**
+     * Creates a ComplexDrink object with given name
+     * 
+     * @param name name of drink
+     */
+    public ComplexDrink(String name) {
         super(name);
-        liquids.add(liquid);
-}
-
-/**
-* Returns volume of all liquids
-*
-* @return the volume of drink in litre
-*/
-@Override
-public double getVolume() {
-    double volume=0;
-    for(Liquid l:liquids){
-        volume+=l.getVolume();
     }
 
     /**
-     * Creates a ComplexDrink object with given name and liquid
+     * Creates a ComplexDrink object with given name and one liquid
      * 
-     * @param name name of drink
+     * @param name   name of drink
+     * @param liquid the desired liquid to add
      */
     public ComplexDrink(String name, Liquid liquid) {
         super(name);
         liquids.add(liquid);
     }
 
-/**
-* Returns alcohol volume percent of all liquids
-*
-* @return alcohol volume percent
-*/
-@Override
-public double getAlcoholPercent() {
-    double percent=0;
-    for(Liquid l:liquids){
-        percent+=l.getAlcoholPercent()*l.getVolume()/getVolume();
+    /**
+     * Creates a ComplexDrink object with given name and list of liquids
+     * 
+     * @param name    name of drink
+     * @param liquids list of liquids
+     */
+    public ComplexDrink(String name, List<Liquid> liquids) {
+        super(name);
+        liquids.addAll(liquids);
     }
-	return Math.round(percent*100)/100d;
-}
 
-/**
-* Gives information if drink is alcoholic or not
-*
-* @return true when alcoholic liquids are present, otherwise false
-*/
-@Override
-public boolean isAlcoholic() {
-   if(getAlcoholPercent()>0){
-       return true;
-   }
-	return false;
-}
+    /**
+     * Returns volume of all liquids
+     *
+     * @return the volume of drink in litre
+     */
+    @Override
+    public double getVolume() {
+        double volume = 0;
+        for (Liquid l : liquids) {
+            volume += l.getVolume();
+        }
+        return volume;
+    }
 
-/**
- * Adds a liquid to the drink
- * 
- * @param liquid the liquid to add
- * @throws TooManyIngredientsException exception could be thrown
- */
-public void addLiquid(Liquid liquid) throws TooManyIngredientsException{
+    /**
+     * Returns alcohol volume percent of the drink
+     *
+     * @return alcohol volume percent
+     */
+    @Override
+    public double getAlcoholPercent() {
+        double percent = 0;
+        for (Liquid l : liquids) {
+            percent += l.getAlcoholPercent() * l.getVolume() / getVolume();
+        }
+        return Math.round(percent * 100) / 100d;
+    }
 
-if(liquids.size()==10){
-    System.out.println("here");
-    throw new TooManyIngredientsException(liquid);
-}else{
-    liquids.add(liquid);}
-}
+    /**
+     * Gives information if drink is alcoholic or not
+     *
+     * @return true when alcoholic liquids are present, otherwise false
+     */
+    @Override
+    public boolean isAlcoholic() {
+        if (getAlcoholPercent() > 0) {
+            return true;
+        }
+        return false;
+    }
 
+    /**
+     * Adds a liquid to the drink
+     * 
+     * @param liquid the liquid to add
+     * @throws TooManyIngredientsException exception could be thrown
+     */
+    public void addLiquid(Liquid liquid) throws TooManyIngredientsException {
+        if (liquids.size() >= 10) {
+            System.out.println("here");
+            throw new TooManyIngredientsException(liquid);
+        } else {
+            liquids.add(liquid);
+        }
+    }
 
     /**
      * Class Exception for the limit of liquids in a drink
      */
-    class TooManyIngredientsException extends Exception {
+}
 
-        public TooManyIngredientsException(Liquid liquid) {
-            super("Drink has too many Ingredients.\nLiquid " + liquid.getName() + " was not added.");
-        }
-
+class TooManyIngredientsException extends Exception {
+    public TooManyIngredientsException(Liquid liquid) {
+        super("Drink has too many Ingredients.\nLiquid " + liquid.getName() + " was not added.");
     }
+
 }
